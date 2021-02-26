06b6aad830f138c53cd89b8f4d55821df671125a
yi_Xu <xjy123456120@163.com>
2021-02-26 12:14:07 +0800

    test: ✅ test input of different type

--- src/index.js
+++ src/index.js
@@ -5,8 +5,20 @@ const src = path.join(__dirname, '..');
 
 async function run() {
   core.startGroup('Get Input value');
-  const license_path = path.join(src, core.getInput('path', { require: false }));
+  const license_path = path.join(src, core.getInput('path'));
   core.info(`license_path: ${license_path}`);
+  const stringInput = core.getInput('stringInput');
+  core.info(`stringInput: ${stringInput}`);
+  core.info(`type of stringInput: ${typeof stringInput}`);
+  const booleanInput = core.getInput('booleanInput');
+  core.info(`booleanInput: ${booleanInput}`);
+  core.info(`type of booleanInput: ${typeof booleanInput}`);
+  const orderMapInput = core.getInput('orderMapInput');
+  core.info(`orderMapInput: ${orderMapInput}`);
+  core.info(`type of orderMapInput: ${typeof orderMapInput}`);
+  const mapInput = core.getInput('mapInput');
+  core.info(`mapInput: ${mapInput}`);
+  core.info(`type of mapInput: ${typeof mapInput}`);
   core.endGroup();
 }
 
