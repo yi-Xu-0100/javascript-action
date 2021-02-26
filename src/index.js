471dc007bc396e794ebcaa17191a874208387290
yi_Xu <xjy123456120@163.com>
2021-02-26 12:25:31 +0800

    use js-yaml to test

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 const core = require('@actions/core');
 const path = require('path');
+const yaml = require('js-yaml');
 
 const src = path.join(__dirname, '..');
 
@@ -7,16 +8,16 @@ async function run() {
   core.startGroup('Get Input value');
   const license_path = path.join(src, core.getInput('path'));
   core.info(`license_path: ${license_path}`);
-  const stringInput = core.getInput('stringInput');
+  const stringInput = yaml.load(core.getInput('stringInput'));
   core.info(`stringInput: ${stringInput}`);
   core.info(`type of stringInput: ${typeof stringInput}`);
-  const booleanInput = core.getInput('booleanInput');
+  const booleanInput = yaml.load(core.getInput('booleanInput'));
   core.info(`booleanInput: ${booleanInput}`);
   core.info(`type of booleanInput: ${typeof booleanInput}`);
-  const orderMapInput = core.getInput('orderMapInput');
+  const orderMapInput = yaml.load(core.getInput('orderMapInput'));
   core.info(`orderMapInput: ${orderMapInput}`);
   core.info(`type of orderMapInput: ${typeof orderMapInput}`);
-  const mapInput = core.getInput('mapInput');
+  const mapInput = yaml.load(core.getInput('mapInput'));
   core.info(`mapInput: ${mapInput}`);
   core.info(`type of mapInput: ${typeof mapInput}`);
   core.endGroup();
